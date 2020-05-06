fd6187f8e6b6cff3ecebcbe59a21850ff1675ede
Tony Grimes <acidtone@tonygrimes.com>
2020-05-06 12:28:27 -0600

    initial commit

--- js/app.mjs
+++ js/app.mjs
@@ -2,13 +2,15 @@ import { phraser } from './phraser.mjs';
 import { colour } from './colour.mjs';
 
 function init() {
+  
   var h1 = document.querySelector('h1');
 
   h1.innerText = phraser.generate(2);
   colour.setRandomHue();
 
-  document.querySelector('body').addEventListener('click', function(){
-    h1.innerText = phraser.generate(2);
+
+  h1.addEventListener('click', function(){
+    this.innerText = phraser.generate(2);
     colour.setRandomHue();
   });
 }
