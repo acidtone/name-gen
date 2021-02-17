2a485410842ca34e6a43f5d84775802280b38823
Tony Grimes <acidtone@tonygrimes.com>
2021-02-17 14:07:26 -0700

    colourizer cleanup

--- js/app.mjs
+++ js/app.mjs
@@ -1,18 +1,13 @@
 import { phraser } from './phraser.mjs';
 import { colour } from './colour.mjs';
 
-// TODO: Do I need an init() function? Probably not.
-function init() {
-  
-  var h1 = document.querySelector('h1');
 
-  h1.innerText = phraser.generate(2);
+var h1 = document.querySelector('h1');
+
+h1.innerText = phraser.generate(2);
+colour.setRandomHue();
+
+h1.addEventListener('click', function(){
+  this.innerText = phraser.generate(2);
   colour.setRandomHue();
-
-  h1.addEventListener('click', function(){
-    this.innerText = phraser.generate(2);
-    colour.setRandomHue();
-  });
-}
-
-document.addEventListener('load',init());
+});
--- js/colour.mjs
+++ js/colour.mjs
@@ -1,31 +1,4 @@
 export const colour = {
-  // TODO: Deprecate this module. Does anything need to be saved?
-  init: function(templateId) {
-    templateId = templateId || false;
-    
-    if (templateId) {
-      // clone and append template
-      const tpl = document.getElementById(templateId);
-      const tplClone = tpl.content.cloneNode(true);
-  
-      const form = tplClone.querySelector('form');
-      const hue = form.hue;
-      const harmony = form.harmony;
-      console.log(harmony);
-  
-      const radios = form.elements["harmony"];
-      for (var i = 0, max = radios.length; i < max; i++) {
-        radios[i].addEventListener('click', function(event) {
-          document.querySelector('body').setAttribute('data-clr-harmony',`${event.target.value}`);
-        })
-      }
-  
-      hue.addEventListener('input', function(event) {
-        document.querySelector(':root').style.setProperty('--hue',`${event.target.value}`);
-      });
-      document.getElementById(templateId).parentNode.appendChild(form);
-    }
-  },
   setRandomHue:  function() {
     const newHue = Math.floor(Math.random() * Math.floor(360));
     document.querySelector(':root').style.setProperty('--hue',newHue);
